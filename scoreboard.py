c817cc992a49e0f9c3c59425ffd9ed4f1b393e7c
Ayush Nautiyal <31501024+nauti09@users.noreply.github.com>
2021-06-25 19:49:08 +0530

    Update scoreboard.py
    For keeping high score even after restarting the game.

--- scoreboard.py
+++ scoreboard.py
@@ -8,20 +8,31 @@ class Scoreboard(Turtle):
         super().__init__()
         self.color("white")
         self.score = 0
+        with open("data.txt", mode="r") as data:
+            self.high_score = int(data.read())
+
         self.penup()
         self.goto(0, 270)
         self.update_scoreboard()
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(arg=f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = str(self.score)
+            with open("data.txt", mode="w") as data:
+                data.write(self.high_score)
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.speed("fastest")
-        self.clear()
         self.update_scoreboard()
